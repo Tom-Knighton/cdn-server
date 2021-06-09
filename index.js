7545d081e190033083d6997a1860759ef12b4e3e
Tom K <tomknighton@icloud.com>
2021-06-09 16:38:43 +0100

    tweak: Rewrite to use single function to write files, using Promises

--- index.js
+++ index.js
@@ -18,47 +18,20 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(require("apikey")(auth, "my realm"));
 
 app.route("/gpchatfeed/:chatuuid").post(function (req, res, next) {
-  var fstream;
   req.pipe(req.busboy);
   req.busboy.on("file", function (fieldname, file, filename) {
-    console.log("Uploading: " + filename);
-    //Path where image will be uploaded
-    fs.promises.mkdir(`/var/www/cdn/GaryPortal/Chat/${req.params.chatuuid}/Attachments`, {
-      recursive: true,
-    });
-    fstream = fs.createWriteStream(
-      `/var/www/cdn/GaryPortal/Chat/${req.params.chatuuid}/Attachments/${filename}`
-    );
-    file.pipe(fstream);
-    fstream.on("close", function () {
-      console.log("Upload Finished of " + filename);
-      res.redirect(
-        `https://cdn.tomk.online/GaryPortal/Chat/${req.params.chatuuid}/Attachments/${filename}`
-      );
-    });
+    writeToLocation(file, filename, `/var/www/cdn/GaryPortal/Chat/${req.params.chatuuid}/Attachments`, `GaryPortal/Chat/${req.params.chatuuid}/Attachments`)
+      .then(url => res.redirect(url))
+      .catch(error => res.send(error, 404));
   });
 });
 
 app.route("/gpprofile/:uuid").post((req, res, next) => {
-  var fstream;
   req.pipe(req.busboy);
   req.busboy.on("file", function (fieldname, file, filename) {
-    console.log("Uploading: " + filename);
-    //Path where image will be uploaded
-    fs.promises.mkdir(
-      `/var/www/cdn/GaryPortal/ProfilePics/${req.params.uuid}`,
-      { recursive: true }
-    );
-    fstream = fs.createWriteStream(
-      `/var/www/cdn/GaryPortal/ProfilePics/${req.params.uuid}/${filename}`
-    );
-    file.pipe(fstream);
-    fstream.on("close", function () {
-      console.log("Upload Finished of " + filename);
-      res.redirect(
-        `https://cdn.tomk.online/GaryPortal/ProfilePics/${req.params.uuid}/${filename}`
-      );
-    });
+    writeToLocation(file, filename, `/var/www/cdn/GaryPortal/ProfilePics/${req.params.uuid}`, `GaryPortal/ProfilePics/${req.params.uuid}`)
+      .then(url => res.redirect(url))
+      .catch(error => res.send(error, 404));
   });
 });
 
@@ -66,21 +39,9 @@ app.route("/gpfeed/media").post((req, res, next) => {
   var fstream;
   req.pipe(req.busboy);
   req.busboy.on("file", function (fieldname, file, filename) {
-    console.log("Uploading: " + filename);
-    //Path where image will be uploaded
-    fs.promises.mkdir(`/var/www/cdn/GaryPortal/Feed/Attachments/Media`, {
-      recursive: true,
-    });
-    fstream = fs.createWriteStream(
-      `/var/www/cdn/GaryPortal/Feed/Attachments/Media/${filename}`
-    );
-    file.pipe(fstream);
-    fstream.on("close", function () {
-      console.log("Upload Finished of " + filename);
-      res.redirect(
-        `https://cdn.tomk.online/GaryPortal/Feed/Attachments/Media/${filename}`
-      );
-    });
+    writeToLocation(file, filename, `/var/www/cdn/GaryPortal/Feed/Attachments/Media`, `GaryPortal/Feed/Attachments/Media`)
+      .then(url => res.redirect(url))
+      .catch(error => res.send(error, 404));
   });
 });
 
@@ -89,34 +50,48 @@ app.route("/gpfeed/aditlog").post((req, res, next) => {
   req.pipe(req.busboy);
   console.log("here");
   req.busboy.on("file", function (fieldname, file, filename) {
-    console.log("Uploading: " + filename);
-    //Path where image will be uploaded
-    fs.promises.mkdir(`/var/www/cdn/GaryPortal/Feed/Attachments/AditLogs`, {
-      recursive: true,
-    });
-    fstream = fs.createWriteStream(
-      `/var/www/cdn/GaryPortal/Feed/Attachments/AditLogs/${filename}`
-    );
-    file.pipe(fstream);
-
-    fstream.on("close", function () {
-      console.log("Upload Finished of " + filename);
-      var proc = new ffmpeg(`/var/www/cdn/GaryPortal/Feed/Attachments/AditLogs/${filename}`).takeScreenshots(
-        {
-          count: 1,
-          timemarks: ["1"], // number of seconds
-          filename: `${filename}.jpg`
-        },
-        "/var/www/cdn/GaryPortal/Feed/Attachments/AditLogs/Thumbs",
-        function (err) {
-          console.log("screenshots were saved");
-        }
-      );
-      res.redirect(
-        `https://cdn.tomk.online/GaryPortal/Feed/Attachments/AditLogs/${filename}`
-      );
-    });
+    writeToLocation(file, filename, `/var/www/cdn/GaryPortal/Feed/Attachments/AditLogs`, `GaryPortal/Feed/Attachments/AditLogs`)
+      .then(url => {
+        var proc = new ffmpeg(`/var/www/cdn/GaryPortal/Feed/Attachments/AditLogs/${filename}`).takeScreenshots(
+          {
+            count: 1,
+            timemarks: ["1"],
+            filename: `${filename}.jpg`
+          },
+          "/var/www/cdn/GaryPortal/Feed/Attachments/AditLogs/Thumbs", function(err) {
+            console.log("Thumbnails saved for AditLog");
+          }
+        );
+        res.redirect(url);
+      })
+      .catch(error => res.send(error, 404));
   });
+});
+
+function writeToLocation(file, fileName, location, urlLocation) {
+  return new Promise((resolve, reject) => {
+    try {
+      fs.promises.mkdir(location, {
+        recursive: true
+      });
+      let fstream = fs.createWriteStream(`${location}/${fileName}`);
+      file.pipe(fstream);
+      fstream.on("close", function() {
+        console.log(`Upload finished of ${fileName}`);
+        resolve(`https://cdn.tomk.online/${urlLocation}/${fileName}`);
+      });
+    }
+    catch(error) {
+      console.log(error);
+      reject(error);
+      return;
+    }
+  });
+};
+
+app.use('/', function (req, res, next) {
+  console.log("received request: " + req.originalUrl + "" + req.method);
+  next();
 });
 
 var server = app.listen(3030, function () {
