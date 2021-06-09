030bde48d8bd105a91aed8e69f9e336df30c3c84
Tom K <tomknighton@icloud.com>
2021-06-09 18:46:23 +0100

    feat: OCR to recognise 'covid' and 'vaccine' words and flag image with IPTC keywords

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var busboy = require("connect-busboy"); //middleware for form/file upload
 var path = require("path"); //used for file path
 var fs = require("fs-extra"); //File System - for file manipulation
 var ffmpeg = require('fluent-ffmpeg');
+const exiftool = require('node-exiftool');
+const ep = new exiftool.ExiftoolProcess();
 var config = require('./config');
+const tesseract = require('node-tesseract-ocr');
 
 function auth(key, fn) {
   if (config.api_key === key) {
@@ -12,6 +15,13 @@ function auth(key, fn) {
     fn(null, null);
   }
 }
+
+const tesseractConfig = {
+  lang: "eng",
+  oem: 1,
+  psm: 3
+};
+
 var app = express();
 app.use(busboy());
 app.use(express.static(path.join(__dirname, "public")));
@@ -79,6 +89,7 @@ function writeToLocation(file, fileName, location, urlLocation) {
       fstream.on("close", function() {
         console.log(`Upload finished of ${fileName}`);
         resolve(`https://cdn.tomk.online/${urlLocation}/${fileName}`);
+        recogniseText(`${location}/${fileName}`);
       });
     }
     catch(error) {
@@ -88,6 +99,37 @@ function writeToLocation(file, fileName, location, urlLocation) {
     }
   });
 };
+
+async function recogniseText(imgLoc) {
+  let keywords = ['Gary Portal'];
+  tesseract.recognize(imgLoc, tesseractConfig)
+    .then(text => {
+      text = text.toLowerCase();
+      const covidWords = ["covid", "c0vid", "c0v1d", "cov1d", "covid-19", "covid 19", "coronavirus", "coronavirus 19", "cov-sars-2", "cov-sars", "cov sars", "cov sars 2", "virus"];
+      const vaccineWords = ["vaccine", "covid vaccine", "covid jab", "vax", "anti-vax", "anti vax", "vaxcine"];
+      if (covidWords.some(word => text.includes(word))) {
+        keywords.push('detectedCovid');
+      }
+      if (vaccineWords.some(word => text.includes(word))) {
+        keywords.push('detectedVaccine');
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => {
+      ep
+      .open()
+      .then(() => ep.writeMetadata(imgLoc, {
+        all: '',
+        comment: 'Gary Portal',
+        'Keywords+': keywords,
+      }, ['overwrite_original']))
+      .then(console.log, console.error)
+      .then(() => ep.close())
+      .catch(console.error);
+    });
+}
 
 app.use('/', function (req, res, next) {
   console.log("received request: " + req.originalUrl + "" + req.method);
